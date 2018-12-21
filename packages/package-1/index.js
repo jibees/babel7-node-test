9669e37126195c99654bbaa9708470f31e298953
Jean-Baptiste Bellet <jean-baptiste.bellet@linkfluence.com>
2018-12-21 15:39:49 +0100

    simplify to isolate issue

--- packages/package-1/index.js
+++ packages/package-1/index.js
@@ -1,10 +1,8 @@
+/*
 import _foo from "@jibees/package1/src/foo";
 import _bar from "@jibees/package1/src/bar/";
 
 export const foo = _foo;
 export const bar = _bar;
-
-export default {
-  foo,
-  bar
-};
+*/
+export default 42;
