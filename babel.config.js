90bebade47b7d999fea7455d9291d99db1192874
Jean-Baptiste Bellet <jean-baptiste.bellet@linkfluence.com>
2018-12-21 15:40:06 +0100

    finally solved my issue

--- babel.config.js
+++ babel.config.js
@@ -26,3 +26,12 @@ module.exports = function(api) {
     plugins
   };
 };
+
+require("@babel/register")({
+  // Find babel.config.js up the folder structure.
+  rootMode: "upward",
+
+  // Since babel ignores all files outside the cwd, it does not compile sibling packages
+  // So rewrite the ignore list to only include node_modules
+  ignore: ["node_modules"]
+});
